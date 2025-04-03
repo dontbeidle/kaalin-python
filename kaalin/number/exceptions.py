0b97e4bb05338bf7e5aa27061bcb939d70e1a120
erinshek <turdibekjumabaev05@gmail.com>
2025-04-03 12:05:56 +0500

    refactor: error message text updated

--- kaalin/number/exceptions.py
+++ kaalin/number/exceptions.py
@@ -1,4 +1,4 @@
 class NumberRangeError(Exception):
-  def __init__(self, message="San shegaradan asıp ketti, maksimum decillion (1 000 000 000 000 000 000 000 000 000 000 000)"):
+  def __init__(self, message="Number exceeded limit"):
     self.message = message
     super().__init__(self.message)
